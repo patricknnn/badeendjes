ba2c1102a8d74adbcb8a1014f648afbf475bfbb3
patricknnn <patrickniewold@gmail.com>
2023-08-07 20:22:32 +0200

    fixed header height

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  public viewPortHeight!: string;
 
+  public constructor() {
+    this.setViewPortHeight();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  private setViewPortHeight(): void {
+    this.viewPortHeight = `${window.innerHeight}px`;
+  }
 }
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -21,10 +21,6 @@ export class SidenavComponent implements AfterViewInit {
   @Input() open: boolean = false;
   @Output() linkClicked: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  public scrollElement: HTMLElement | null = null;
-
-  public constructor(private readonly _changeDetector: ChangeDetectorRef) {}
-
   public navlinks: navLink[] = [
     { title: 'home.title', href: '#header' },
     { title: 'welcome.title', href: '#welcome' },
@@ -32,27 +28,24 @@ export class SidenavComponent implements AfterViewInit {
     { title: 'about.title', href: '#about' },
     { title: 'contact.title', href: '#contact' },
   ];
+  
+  public constructor(private readonly _changeDetector: ChangeDetectorRef) {}
 
   public ngAfterViewInit(): void {
-    this.scrollElement = document.getElementById('scroll-content');
-
     this._changeDetector.detectChanges();
   }
 
   public isNavlinkActive(id: string): boolean {
-    if (this.scrollElement) {
-      const element = document.getElementById(id.substring(1));
+    const scrollElement = document.getElementById('scroll-content');
+    const element = document.getElementById(id.substring(1));
 
-      if (element) {
-        const rect: DOMRect = element.getBoundingClientRect();
+    if (scrollElement && element) {
+      const rect: DOMRect = element.getBoundingClientRect();
 
-        return (
-          this.scrollElement.offsetHeight + rect.height > rect.bottom &&
-          this.scrollElement.offsetHeight < rect.bottom
-        );
-      }
-
-      return false;
+      return (
+        scrollElement.offsetHeight + rect.height > rect.bottom &&
+        scrollElement.offsetHeight < rect.bottom
+      );
     }
 
     return false;
