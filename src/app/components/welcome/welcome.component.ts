a7ac3c8029842d044fadfdff66a66a4323f3f5f0
patricknnn <patrickniewold@gmail.com>
2023-08-30 18:05:58 +0200

    updated text and added house rules

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent {
-
+  public houseRules: string =
+    './assets/files/Huisregels Kinderopvang De Badeendjes.pdf';
 }
